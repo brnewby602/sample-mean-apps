0247d51282a1b80f6129386119069a498a0aa14e
Bryan Newby <brnewby602@gmail.com>
2016-04-05 14:08:13 -0700

    Add new object parameter for favorite activity

--- manatee-tracker/server/data/sampleManateeData.js
+++ manatee-tracker/server/data/sampleManateeData.js
@@ -4,14 +4,16 @@ var manateeData = [
     "weight": 1200,
     "age": 43,
     "disposition": "Blood thirsty",
-    "imageUrl": "https://christopherjgervais.files.wordpress.com/2016/01/368375.jpg?w=493&h=309"
+    "imageUrl": "https://christopherjgervais.files.wordpress.com/2016/01/368375.jpg?w=493&h=309",
+    "favorite": "Ramming boats at full speed"
   },
   {
     "name": "Mister Whiskers",
     "weight": 2100,
     "age": 31,
     "disposition": "Docile",
-    "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/1/1e/Manatee_at_Sea_World_Orlando_Mar_10.JPG"
+    "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/1/1e/Manatee_at_Sea_World_Orlando_Mar_10.JPG",
+    "favorite": "Eating"
   }
 ];
 
