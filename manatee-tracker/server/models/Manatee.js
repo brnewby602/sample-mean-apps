b60be0cf7f7c8703ede0cf759d7bdd50a9bc9de8
Bryan Newby <brnewby602@gmail.com>
2016-04-05 14:09:12 -0700

    Add favorite property to the schema

--- manatee-tracker/server/models/Manatee.js
+++ manatee-tracker/server/models/Manatee.js
@@ -8,12 +8,12 @@ Example data:
     "name": "Mr. Wiskers",
     "weight": 1200,
     "age": 53,
-    "disposition": "Homicidal Rage",
-    "imageURL": "https://christopherjgervais.files.wordpress.com/2016/01/368375.jpg?w=493&h=309"
+    "disposition": "Ornery",
+    "imageURL": "https://christopherjgervais.files.wordpress.com/2016/01/368375.jpg?w=493&h=309",
+    "favorite": "Eating"
   }
 
 */
-
 
 var manateeSchema = Schema({
   name: {
@@ -23,7 +23,8 @@ var manateeSchema = Schema({
   weight: Number,
   age: Number,
   disposition: String,
-  imageUrl: String
+  imageUrl: String,
+  favorite: String
 });
 
 var Manatee = mongoose.model('Manatee', manateeSchema) ;
