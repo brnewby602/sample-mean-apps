1adbc4367992c729135745a8d19898b9b9e47f53
Bryan Newby <brnewby602@gmail.com>
2016-04-05 12:01:03 -0700

    Update sample data, create style for profile picture

--- manatee-tracker/server/data/sampleManateeData.js
+++ manatee-tracker/server/data/sampleManateeData.js
@@ -2,16 +2,16 @@ var manateeData = [
   {
     "name": "Tubs",
     "weight": 1200,
-    "age": 53,
+    "age": 43,
     "disposition": "Blood thirsty",
     "imageUrl": "https://christopherjgervais.files.wordpress.com/2016/01/368375.jpg?w=493&h=309"
   },
   {
-    "name": "Mr. Wiskers",
-    "weight": 822,
+    "name": "Mister Whiskers",
+    "weight": 2100,
     "age": 31,
     "disposition": "Docile",
-    "imageUrl": "https://christopherjgervais.files.wordpress.com/2016/01/368375.jpg?w=493&h=309"
+    "imageUrl": "https://upload.wikimedia.org/wikipedia/commons/1/1e/Manatee_at_Sea_World_Orlando_Mar_10.JPG"
   }
 ];
 
